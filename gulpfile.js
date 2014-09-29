f35089ed05b4f8a7b867ef19ed50825c8c0e3d96
bangarang <me@alexjrock.com>
2014-09-29 15:36:53 -0500

    Hew Homepage Layout

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+// gulpfile.js
+// Include gulp
+var gulp = require('gulp'); 
+
+// Include Our Plugins
+var jshint = require('gulp-jshint'),
+		sass = require('gulp-sass'),
+		concat = require('gulp-concat'),
+		uglify = require('gulp-uglify'),
+		rename = require('gulp-rename'),
+		nodemon = require('gulp-nodemon');
+
+// Lint Task
+gulp.task('lint', function() {
+    return gulp.src('js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+// Compile Our Sass
+gulp.task('sass', function() {
+    return gulp.src('sass/css/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('assets/css'));
+});
+
+// Concatenate & Minify JS
+// gulp.task('scripts', function() {
+//     return gulp.src('js/*.js')
+//         .pipe(concat('all.js'))
+//         .pipe(gulp.dest('dist'))
+//         .pipe(rename('all.min.js'))
+//         .pipe(uglify())
+//         .pipe(gulp.dest('dist'));
+// });
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+    // gulp.watch('js/*.js', ['lint', 'scripts']);
+    gulp.watch('sass/css/*.scss', ['sass']);
+});
+
+gulp.task('develop', function () {
+  nodemon({ script: 'server.js', ext: 'html js', ignore: ['ignored.js'] })
+    .on('change', ['lint'])
+    .on('restart', function () {
+      console.log('restarted!')
+    })
+})
+
+// Default Task
+// gulp.task('default', ['lint', 'sass', 'scripts', 'watch']);
+gulp.task('default', ['sass', 'watch', 'develop']);
